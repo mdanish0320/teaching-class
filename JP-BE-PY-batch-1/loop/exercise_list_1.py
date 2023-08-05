d5346b192c0bd585fd4d63757474c30641539149
mdanish0320 <mdanish0320@gmail.com>
2023-08-05 21:50:00 +0500

    class-4

---
+++ JP-BE-PY-batch-1/loop/exercise_list_1.py
@@ -0,0 +1,59 @@
+# 1. output the numbers from 1 to 10 using range function and for loop
+# output should be like
+# 1
+# 2
+# 3
+# etc
+
+# 2. output the numbers from 35 to 50 using range function and for loop
+# output should be like
+# 35
+# 36
+# 37
+# etc
+
+
+# 3. output the numbers from -15 to -25 using range function and for loop
+# output should be like
+# -15
+# -16
+# -17
+# etc
+
+# 4. output the numbers from 5 to -10 using range function and for loop
+# output should be like
+# 5
+# 4
+# 3
+# etc
+
+# 5. output the numbers from 0 to 50 incremented by 3 using range function and for loop
+# output should be like
+# 0
+# 3
+# 6
+# 9
+# etc
+
+# 6.  Write a program to Generate Multiplication Table of 2 using range function and for loop
+# output format should be like
+# 2 x 1 = 2
+# 2 x 2 = 4
+# etc
+
+
+# 7. Write a Python program to sum all the items in a list use for loop. consider the list [3, 5, 2, 1, 4]
+# output should be 15
+# hint: 
+# create a variable x outside the loop and assign the value 0
+# inside the loop increment the value of x with the local variable of loop
+# x += i
+
+
+# 8. Write a Python program to get the largest number from a list and use for loop consider the list [3, 5, 2, 1, 4]
+# output should be 5
+# hint:
+# create a variable x outside the loop and assign the value 0
+# inside the loop compare the variable x with the local variable of loop
+
+
