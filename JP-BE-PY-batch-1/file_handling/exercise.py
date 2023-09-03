431882887ae4e5ad999d9ebc064f6e9b12271cc7
mdanish0320 <mdanish0320@gmail.com>
2023-09-03 12:48:29 +0500

    updated exercise of file handling

--- JP-BE-PY-batch-1/file_handling/exercise.py
+++ JP-BE-PY-batch-1/file_handling/exercise.py
@@ -12,3 +12,21 @@ Please click *here* to open the article
 
 # after running the program three times
 # the file user_email.txt must have all three users content
+
+
+# Create a new text file named "student_records.txt" with the following initial content:
+# Student ID | Student Name | Grade
+# 101       | Alice        | A
+# 102       | Bob          | B
+# 103       | Carol        | C
+
+# Open the "student_records.txt" file in read mode ('r') and read its contents line by line. Print each line to the console.
+
+# Create a new text file named "updated_records.txt" in write mode('w').
+# Read the content of "student_records.txt" again and write only the lines containing students with grades 'A' or 'B' to the "updated_records.txt" file.
+# Close both files.
+
+# Open "updated_records.txt" in append mode('a') and add a new student record:
+# Close the "updated_records.txt" file.
+
+# Open "updated_records.txt" in read mode and print its contents to the console to verify that the new student record has been added.
