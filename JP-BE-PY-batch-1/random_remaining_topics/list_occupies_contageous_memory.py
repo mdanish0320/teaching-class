7de2eb075e4415b246166e202bb2334e15d4c5e7
mdanish0320 <mdanish0320@gmail.com>
2023-09-03 12:36:16 +0500

    update list meory example

--- JP-BE-PY-batch-1/random_remaining_topics/list_occupies_contageous_memory.py
+++ JP-BE-PY-batch-1/random_remaining_topics/list_occupies_contageous_memory.py
@@ -4,35 +4,44 @@ import sys
 my_list = []
 
 my_list.append(1)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
+print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes at memory location {id(my_list)}")
 
 my_list.append(2)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
+print(
+    f"Memory usage of list: {sys.getsizeof(my_list)} bytes at memory location {id(my_list)}")
 
 my_list.append(3)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
+print(
+    f"Memory usage of list: {sys.getsizeof(my_list)} bytes memory location {id(my_list)}")
 
 my_list.append(4)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
+print(
+    f"Memory usage of list: {sys.getsizeof(my_list)} bytes memory location {id(my_list)}")
 
 my_list.append(5)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
+print(
+    f"Memory usage of list: {sys.getsizeof(my_list)} bytes memory location {id(my_list)}")
 
 my_list.append(6)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
+print(
+    f"Memory usage of list: {sys.getsizeof(my_list)} bytes memory location {id(my_list)}")
 
 my_list.append(7)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
+print(
+    f"Memory usage of list: {sys.getsizeof(my_list)} bytes memory location {id(my_list)}")
 
 my_list.append(8)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
+print(
+    f"Memory usage of list: {sys.getsizeof(my_list)} bytes memory location {id(my_list)}")
 
 my_list.append(9)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
+print(
+    f"Memory usage of list: {sys.getsizeof(my_list)} bytes memory location {id(my_list)}")
 
 my_list.append(10)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
+print(
+    f"Memory usage of list: {sys.getsizeof(my_list)} bytes memory location {id(my_list)}")
 
 my_list.append(11)
-print(f"Memory usage of list: {sys.getsizeof(my_list)} bytes")
-
+print(
+    f"Memory usage of list: {sys.getsizeof(my_list)} bytes memory location {id(my_list)}")
