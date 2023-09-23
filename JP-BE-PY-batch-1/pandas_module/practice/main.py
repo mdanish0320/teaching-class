4774da4323148c85bffa9261cc92d6c7673b26e2
mdanish0320 <mdanish0320@gmail.com>
2023-09-23 15:15:50 +0500

    done

--- JP-BE-PY-batch-1/pandas_module/practice/main.py
+++ JP-BE-PY-batch-1/pandas_module/practice/main.py
@@ -23,12 +23,12 @@ df = pd.read_csv('pokemon_data.csv')
 
 
 
+
 # # single value 
 # print(df.loc[0, 'Name'])
 
 # # single but specific whole row
 # print(df.loc[0]) # Label of Column
-
 
 
 # # single but specific whole column
@@ -65,6 +65,14 @@ df = pd.read_csv('pokemon_data.csv')
 
 # new_df = df[ ~df['Type 2'].isin(['Poison', 'Water', 'Flying', 'Dark', 'Fairy', 'Dragon', 'Ground', np.nan, 'Grass', 'Fighting', 'Normal']) ]
 # print(len(new_df))
+
+# # str contains
+# df.loc[df[‘Name’].str.contains(“Mega”)]
+# df.loc[~df[‘Name’].str.contains(“Mega”)
+# df.loc[~df[‘Name’].str.contains(“Fire | Grass”, regex=True)
+# df.loc[~df[‘Name’].str.contains(“Fire | Grass”, flags=re.I, regex=True)  # case insensitive
+# df.loc[~df[‘Name’].str.contains(“^ pi[A-Z] *”, flags=re.I, regex=True)  # case insensitive
+
 
 
 
@@ -135,17 +143,66 @@ df = pd.read_csv('pokemon_data.csv')
 # print(new_df.head(20))
 
 
-# Modify
-# join,  Merge, Join
-# str contains
+# # Modify
+# # update column names
+## all
+# df.columns = ['col1', 'col2', 'col3']
+## selected
+# df.rename(columns={'col1': 'col_1', 'col2': 'col_2'})
+
+# # # update selected 1 complete row
+# df.loc[0] = ['val_1', 'val_2', 'val_3']
+# # update few column in selected row
+# df.loc[0, ['col_1', 'col_2']] = ['val_1', 'val_2']
+
+# # # update whole column
+# df['email'] = df['email'].str.lower()
+
+# # # conditional update
+# filt = (df['email' == 'danish@abc.com'])
+# df.loc[filt, 'email'] = 'danish@circadia.health'
+
+# # convert str to datetime
+# df['date'] = pd.to_datetime(df['date'])
+# filtering based on date
+# df[(df['date'] > '2013-01-01') & (df['date'] < '2013-02-01')]
+
+# df.drop('col_name', axis=1) # delete column 
+# df.drop(index_name, axis=0) # delete row named “0”
+
+
+
+# join,  Merge, combine
+
 # deleting duplicates
 
 # example data
 # https://github.com/KeithGalli/Pandas-Data-Science-Tasks/tree/master/SalesAnalysis/Sales_Data
 
 
+dff = pd.read_csv("../data_generator/employee_data.csv")
+# print(dff.nunique()['Department'])
+
+## count strenth of department
+# print(dff.groupby("Department").count())
+
+# view retention of each department # konsa depart zyada khush hai
+# print(dff.groupby("Department").mean()['YearsOfService'])
+
+## average age of each department
+# print(dff.groupby("Department").mean()['Age'])
+# print(dff.groupby("Department").agg(['max', 'min', 'mean'])['Age'])
+
+# # multiple groups
+# count male and female in each department
+print(dff.groupby(["Department", "Gender"]).count())
+# print(dff.groupby(["Department"]).nunique()['Gender'])
+# print(dff.groupby(["Department", 'Gender']).nunique())
+
+# findout the salary of male and female in each group
+# print(dff.groupby(["Department", "Gender"]).sum()['Salary'])
 
 
-
-
+# pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_columns', None)
 
