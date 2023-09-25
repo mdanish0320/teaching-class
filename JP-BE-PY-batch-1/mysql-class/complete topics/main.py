057be7c42a0b507a78e796ca768be0dabec2e6ee
Danish <mdanish0320@gmail.com>
2023-09-25 19:22:35 +0500

    full content

---
+++ JP-BE-PY-batch-1/mysql-class/complete topics/main.py
@@ -0,0 +1,245 @@
+"""
+Getting Started(25m)
+Introduction
+What is SQL
+Installing MySQL on Mac
+Installing MySQL on Windows
+Creating the Databases
+What You'll Learn
+Follow Me Around
+Learning Paths
+
+
+Retrieving Data From a Single Table(60m)
+The SELECT Statement
+
+The SELECT Clause
+The WHERE Clause
+The AND, OR and NOT Operators
+The IN Operator
+The BETWEEN Operator
+The LIKE Operator
+The REGEXP Operator
+The IS NULL Operator
+The ORDER BY Clause
+The LIMIT Clause
+
+
+
+Retrieving Data From Multiple Tables(1h)
+Inner Joins
+Joining Across Databases
+Self Joins
+Joining Multiple Tables
+Compound Join Conditions
+Implicit Join Syntax
+Outer Joins
+Outer Join Between Multiple Tables
+Self Outer Joins
+The USING Clause
+Natural Joins
+Cross Joins
+Unions
+A Quick Note
+
+
+Inserting, Updating, and Deleting Data(42m)
+Column Attributes
+Inserting a Row
+Inserting Multiple Rows
+Inserting Hierarchical Rows
+Creating a Copy of a Table
+Updating a Single Row
+Updating Multiple Rows
+Using Subqueries in Updates
+Deleting Rows
+Restoring the Databases
+Column Attributes
+Inserting a Row
+Inserting Multiple Rows
+Inserting Hierarchical Rows
+Creating a Copy of a Table
+Updating a Single Row
+Updating Multiple Rows
+Using Subqueries in Updates
+Deleting Rows
+Restoring the Databases
+
+
+
+
+Summarizing Data(31m)
+Aggregate Functions
+The GROUP BY Clause
+The HAVING Clause
+The ROLLUP Operator
+
+
+
+Writing Complex Query(39m)
+Introduction
+Subqueries
+The IN Operator
+Subqueries vs Joins
+The ALL Keyword
+The ANY Keyword
+Correlated Subqueries
+The EXISTS Operator
+Subqueries in the SELECT Clause
+Subqueries in the FROM Clause
+
+
+
+Essential MySQL Functions(32m)
+Numeric Functions
+String Functions
+Date Functions in MySQL
+Formatting Dates and Times
+Calculating Dates and Times
+The IFNULL and COALESCE Functions
+The IF Function
+The CASE Operator
+
+
+
+Views(19m)
+Creating Views
+Altering or Dropping Views
+Updatable Views
+THE WITH OPTION CHECK Clause
+Other Benefits of Views
+
+
+
+Stored Procedures(49m)
+What are Stored Procedures
+Creating a Stored Procedure
+Creating Procedures Using MySQLWorkbench
+Dropping Stored Procedures
+Parameters
+Parameters with Default Value
+Parameter Validation
+Output Parameters
+Variables
+Functions
+Other Conventions
+
+
+
+Triggers and Events(21m)
+Triggers
+Viewing Triggers
+Dropping Triggers
+Using Triggers for Auditing
+Events
+Viewing, Dropping and Altering Events
+
+
+
+Transactions and Concurrency(44m)
+Transactions
+Creating Transactions
+Concurrency and Locking
+Concurrency Problems
+Transaction Isolation Levels
+READ UNCOMMITTED Isolation Level
+READ COMMITTED Isolation Level
+REPEATABLE READ Isolation Level
+SERIALIZABLE Isolation Level
+Deadlocks
+
+
+
+Data Types(24m)
+Introduction
+String Types
+Integer Types
+Fixed-point and Floating-point Types
+Boolean Types
+Enum and Set Types
+Date and Time Types
+Blob Types
+JSON Type
+
+
+
+Designing Databases(2h)
+Introduction
+Data Modelling
+Conceptual Models
+Logical Models
+Physical Models
+Primary Keys
+Foreign Keys
+Foreign Key Constraints
+Normalization
+1NF- First Normal Form
+Link Tables
+2NF- Second Normal Form
+3NF- Third Normal Form
+My Pragmatic Advice
+Don't Model the Universe
+Forward Engineering a Model
+Synchronizing a Model with a Database
+Reverse Engineering a Database
+Project- Flight Booking System
+Solution- Conceptual Model
+Solution- Logical Model
+Project - Video Rental Application
+Solution- Conceptual Model
+Solution- Logical Model
+Creating and Dropping Databases
+Creating Tables
+Altering Tables
+Creating Relationships
+Altering Primary and Foreign Key Constraints
+Character Sets and Collations
+Storage Engines
+A Quick Note
+
+
+Indexing for High Performance(53m)
+Introduction
+Indexes
+
+Creating Indexes
+
+Viewing Indexes
+
+Prefix Indexes
+
+Full-text Indexes
+
+Composite Indexes
+
+Order of Columns in Composite Indexes
+
+When Indexes are Ignored
+
+Using Indexes for Sorting
+
+Covering Indexese
+
+Index Maintenance
+
+Performance Best Practices
+
+
+Securing Databases(15m)
+Introduction
+
+Creating a User
+
+Viewing Users
+
+Dropping Users
+
+Changing Passwords
+
+Granting Privileges
+
+Viewing Privileges
+
+Revoking Privileges
+
+"""
---
+++ JP-BE-PY-batch-1/mysql-class/content.py
@@ -0,0 +1,124 @@
+# install mysql workbench
+# connect to database
+# import db file
+# relational database
+# sql is known of declarative programming langugage - non-procedural
+# structure Query Language
+# - you focused on what and not how part
+# main components
+# - DDL -> table level - create, alter, drop
+# - DML -> data level - select, insert, update, delete
+# - DCL -> Grant 
+# - TCL -> commit, rollback
+
+# RDBMS
+    # Mysql - open sourced
+    # Sql Server
+    # Oracle
+    # etc
+
+# table name singular or plural
+# column name singular or plural
+
+# case insensitive command
+# semicolon 
+
+# Schema
+
+# Data Normalization
+    # 1NF
+    # 2NF
+    # 3NF
+    # 4BCNF
+
+
+# constraint
+    # column NOT NULL
+    # primary key
+    # composite key
+    # auto increment
+
+# column alias
+# order by 
+# limit
+# where
+    # And
+    # or
+    # between
+    # is null
+    # like
+    # date comparison
+
+# data types
+    # int
+    # float
+    # string
+    # date and datetime
+    # blob
+    # text
+
+# function
+    # distinct
+    # date(), MONTH(), YEAR()
+
+# Group By
+    # having
+
+
+# join
+
+# delete rows
+
+# exercise: https://github.com/WebDevSimplified/Learn-SQL
+
+
+# data dump: https://bit.ly/3rvtqdO
+
+
+# relationship
+# sub queries
+
+# what is database
+# collection of data stored in a format that can easily be accessed
+
+
+# Query Execution Order
+
+# Topics
+# Use Database
+# Select * from table
+# Select few columns from table
+# select * from table limit 10
+# select * from table order by id ASC or DESC
+
+# WHERE
+# single condition
+# multip condition
+# not condition
+# NULL condition
+# IN, NOT In Operator
+# LIKE
+
+
+# JOIN
+# inner join
+# left join
+# right join
+# outer join
+# cross join
+# multiple join
+
+
+# insert single
+# insert multiple
+# insert into select from
+
+# update table
+# delete from table
+
+
+# Table level queries
+
+
+# Arithmatic Operation i.e addition and subtraction
+
