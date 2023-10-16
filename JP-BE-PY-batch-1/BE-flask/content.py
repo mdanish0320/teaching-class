fb64fdf6a299f81708afe753346c64e2695f7c37
Muhammad Danish <mdanish0320@gmail.com>
2023-10-16 13:15:29 +0500

    Create content.py

---
+++ JP-BE-PY-batch-1/BE-flask/content.py
@@ -0,0 +1,31 @@
+# install flask
+# run server
+# explain server port (again)
+# create APIs without class view or blueprint
+# use postman to invoke api
+# explain HTTP Methods
+# Create CRUD in-memory
+
+# small project management
+# create apis with view as function
+# create apis with class view
+
+# big project magagement
+# create apis with blueprint with view fuction
+  # # Blueprint
+  # give you a feature to group endpoints and then you can add that group in the flask app
+# create apis with blueprint with class view
+
+# Data Validation + Swagger API Documention
+# flask-smorest Requires: Python >=3.8
+
+# differnet extensions fo Flask API
+flask-restplus is a fork of flask-restful
+restplus is unmainted and a new forked repo is named as Flask-RESTX 
+flask-smorest (formerly known as flask-rest-api) 
+
+flask-smorest vs Flask-RESTX 
+
+Flask-RESTX requires Python 3.7+.
+flask-smorest Requires: Python >=3.8
+
