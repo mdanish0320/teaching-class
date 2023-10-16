86b364f1e01ed7ed313d0ec2e1a9cb7abec33d4a
Muhammad Danish <mdanish0320@gmail.com>
2023-10-16 17:04:09 +0500

    Update content.py

--- JP-BE-PY-batch-1/BE-flask/content.py
+++ JP-BE-PY-batch-1/BE-flask/content.py
@@ -19,6 +19,31 @@
 # Data Validation + Swagger API Documention
 # flask-smorest Requires: Python >=3.8
 
+## Concept
+# OSI Model
+# TCP Connection and its possible values
+# TCP -> HTTP, Text, Email, video streaming services
+# UTCP -> VoIP, Live video Streaming, DNS, Online Games
+# HTTP Requests/Response - https://www.youtube.com/watch?v=76CcJ90Lz4U
+# General headers
+# Http Request Method: get, post, put, delete
+  # POST: https://www.atatus.com/blog/http-post-request-method/
+  # Authorization: https://compile7.org/decompile/authorization-request-headers-explained/
+  # status code
+  # image: https://www.seobility.net/en/wiki/images/d/d2/HTTP-Header.png
+  # image: https://mkyong.com/wp-content/uploads/2016/01/view-header-in-chrome.png
+# HTTP Reponse Header:
+  # Access-Control-Allow-Origin
+  # Content-Type
+# Diff between POST and PUT: POST supposed to create new record in db and PUT doesn’t. Calling PUT API multiple times should not create any new record in db but calling POST API multiple times tends to create new record in db.
+# Content types 
+# Web services 
+  # Rest vs soap
+  # Rest vs restful - https://www.youtube.com/watch?v=UJgq1tEwMBk
+
+
+
+
 # differnet extensions fo Flask API
 flask-restplus is a fork of flask-restful
 restplus is unmainted and a new forked repo is named as Flask-RESTX 
