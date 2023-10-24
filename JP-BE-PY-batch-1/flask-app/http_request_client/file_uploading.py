9b0b947c5527769c06e5a9b50ccdb704ab84c757
Danish <mdanish0320@gmail.com>
2023-10-24 13:37:42 +0500

    upload multiple files

--- JP-BE-PY-batch-1/flask-app/http_request_client/file_uploading.py
+++ JP-BE-PY-batch-1/flask-app/http_request_client/file_uploading.py
@@ -1,9 +1,14 @@
 import requests
 
-url = 'http://localhost:3000/input/post/form-data/upload-files'
+url = 'http://localhost:3000/input/post/form-data/upload-file'
 fp = '/Users/danish/Desktop/000.json'
-
 files = {'my_json_file': open(fp, 'rb')}
+
+# url = 'http://localhost:3000/input/post/form-data/upload-multiple-files'
+# fp = '/Users/danish/Desktop/000.json'
+# files = [('my_json_file', ('foo.json', open(fp, 'rb'))), ('my_json_file', ('bar.json', open(fp, 'rb')))]
+
+
 payload = {'file_id': '1234'}
 
 response = requests.post(url, files=files, data=payload, verify=False)
