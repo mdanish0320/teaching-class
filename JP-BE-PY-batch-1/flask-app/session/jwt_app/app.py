bbb5ef7f5ba7268eab54c869d24cf0a737dfec8d
Danish <mdanish0320@gmail.com>
2023-11-10 21:52:53 +0500

    add jwt token session

---
+++ JP-BE-PY-batch-1/flask-app/session/jwt_app/app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask import request
+from datetime import timedelta
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+
+# https://flask-jwt-extended.readthedocs.io/en/stable/options.html
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+app.config["JWT_ALGORITHM"] = "HS256"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
+
+jwt = JWTManager(app)
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return {"msg": "Bad username or password"}, 401
+
+    access_token = create_access_token(identity=username)
+    return {'access_token':access_token}, 200
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return {'logged_in_as':current_user}, 200
+
+
+if __name__ == "__main__":
+    app.run(
+        debug= True
+    )
