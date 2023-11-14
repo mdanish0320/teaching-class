df1e27d3fa7f3f27593689e77a20cd69471267cf
Danish <mdanish0320@gmail.com>
2023-11-14 13:17:00 +0500

    remove comments

--- JP-BE-PY-batch-1/flask-app/route_management/method_view_class_2/tests/test_employee.py
+++ JP-BE-PY-batch-1/flask-app/route_management/method_view_class_2/tests/test_employee.py
@@ -12,20 +12,20 @@ def random_char(char_num):
 
 
 # pytest -ssv
-# def test_list_employees():
-#     e = EmployeeView(
-#         {},
-#         {},
-#         {},
-#     )
+def test_list_employees():
+    e = EmployeeView(
+        {},
+        {},
+        {},
+    )
 
-#     print(e.test_this_function())
+    print(e.test_this_function())
 
-# def test_list_employee_api():
-#     response = app.test_client().get("/employee")
-#     print(response.status_code)
-#     print(response.json)
-#     print(response.data)
+def test_list_employee_api():
+    response = app.test_client().get("/employee")
+    print(response.status_code)
+    print(response.json)
+    print(response.data)
 
 def test_search_in_list_employee_api():
     response = app.test_client().get("/employee", query_string={"name": "danish", "age": 30})
@@ -35,21 +35,21 @@ def test_search_in_list_employee_api():
     print(response.json)
     print(response.data)
 
-# def test_create_employee_api():
-#     response = app.test_client().post("/employee", json={
-#     "fname": "muhammad", 
-#     "lname": "danish",
-#     "email": random_char(5)+"@gmail.com"
-#     })
-#     print(response.status_code)
-#     print(response.data)
-#     print(response.json)
+def test_create_employee_api():
+    response = app.test_client().post("/employee", json={
+    "fname": "muhammad", 
+    "lname": "danish",
+    "email": random_char(5)+"@gmail.com"
+    })
+    print(response.status_code)
+    print(response.data)
+    print(response.json)
 
 
-# def test_get_employee_profile_api():
-#     response = app.test_client().get("/employee/1")
-#     print(response.status_code)
-#     print(response.data)
-#     print(response.json)
+def test_get_employee_profile_api():
+    response = app.test_client().get("/employee/1")
+    print(response.status_code)
+    print(response.data)
+    print(response.json)
 
 
