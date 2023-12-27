a22008812c952798ffcbeecab94e295380427226
Danish <mdanish0320@gmail.com>
2023-12-27 21:21:55 +0500

    add simplejwt for login

--- JP-BE-PY-batch-1/django-app/drf_blog/account/views.py
+++ JP-BE-PY-batch-1/django-app/drf_blog/account/views.py
@@ -3,12 +3,19 @@ from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.permissions import IsAuthenticated
+
+# from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .serializers import UserRegisterSerializer, AdminRegisterSerializer
 
 # Create your views here.
 class RegisterUser(APIView):
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
     serializer = UserRegisterSerializer
+
     def post(self, request):
         serialized_user = self.serializer(data=request.data)
         if serialized_user.is_valid(raise_exception=True):
@@ -23,9 +30,3 @@ class RegisterAdmin(APIView):
         if serialized_user.is_valid(raise_exception=True):
             serialized_user.save()
             return Response({"message": "admin user success"})
-
-
-class LoginUser(APIView):
-    def post(self, request):
-        print("register user", request.data)
-        return Response({"message": "login success"})
--- JP-BE-PY-batch-1/django-app/drf_blog/main/settings.py
+++ JP-BE-PY-batch-1/django-app/drf_blog/main/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
