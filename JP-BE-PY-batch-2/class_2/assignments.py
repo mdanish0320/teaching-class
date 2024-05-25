c3e39029d1cbeba4974082e8cdf8ef649383ab63
mdanish0320 <mdanish0320@gmail.com>
2024-05-25 19:45:09 +0500

    class 2 assignment update

--- JP-BE-PY-batch-2/class_2/assignments.py
+++ JP-BE-PY-batch-2/class_2/assignments.py
@@ -84,3 +84,16 @@ and split the sentence into words.
 
 NOTE: Concepts Covered: replace(), split(), input(), print()
 """
+
+# problem 9
+"""
+# break the following sentences
+
+"This is a sample sentence."
+"Alice,Bob,Charlie,David"
+"2023-09-01"
+"Line 1
+Line 2
+Line 3"
+"https://www.example.com/path/to/resource"
+"""
