ca257f5f33201e74b22af028e42d4d87a17914c7
mdanish0320 <mdanish0320@gmail.com>
2024-07-27 19:40:50 +0500

    class 12 solid + cpu

--- JP-BE-PY-batch-2/class_12/cpu_vs_io_bound_tasks.py
+++ JP-BE-PY-batch-2/class_12/cpu_vs_io_bound_tasks.py
@@ -1,1 +1,21 @@
 # https://medium.com/@sim30217/cpu-bound-i-o-bound-ea3c92499e4c
+
+"""
+CPU bound tasks
+1. Image, Video and Audio Processing
+2. Data Encryption and Decryption
+3. Data Compression and Decompression
+4. Parsing and Serializing Large JSON Objects
+5. Cryptographic Hashing
+6. Machine Learning and AI Tasks
+"""
+
+
+"""
+I/O bound tasks
+1. Reading from or Writing to Files
+2. Network Communication i.e HTTP
+3. Database Operations
+4. User Input/Output
+5. Inter-process Communication: between two apps in same or different machine
+"""
---
+++ JP-BE-PY-batch-2/class_12/how_cpu_works.py
@@ -0,0 +1,4 @@
+# https://www.youtube.com/watch?v=FZGugFqdr60
+# https://www.youtube.com/watch?v=zltgXvg6r3k
+# https://www.youtube.com/watch?v=qYPYjpwXpoA
+# https://www.youtube.com/watch?v=3PcO10iAXTk
