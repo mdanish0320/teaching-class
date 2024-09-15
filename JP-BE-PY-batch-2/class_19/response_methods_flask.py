e24ac1b3caa93b2b70819996a9f07c60599e6a96
mdanish0320 <mdanish0320@gmail.com>
2024-09-15 13:23:30 +0500

    class 19

---
+++ JP-BE-PY-batch-2/class_19/response_methods_flask.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, Response, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/error')
+def error_example():
+    return 'Not Found', 404
+
+
+@app.route('/custom')
+def custom_response():
+    return 'Custom Response', 200, {'X-Custom-Header': 'Value'}
+
+
+@app.route('/data')
+def get_data():
+    data = {'key': 'value'}
+    return jsonify(data)
+
+
+@app.route('/response')
+def custom_response_object():
+    # entire response object in one go.
+    return Response('Custom Response Object', status=200, mimetype='text/plain')
+
+
+@app.route('/json_make_response')
+def json_make_response():
+    data = {'key': 'value'}
+    response = make_response(jsonify(data))
+    response.headers['X-Custom-Header'] = 'Value'
+    return response
+
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        port=3000
+    )
