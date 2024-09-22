be35fd0b558e1a65de6a000742d4589b636513fb
Danish <mdanish0320@gmail.com>
2024-09-22 19:54:12 +0500

    remove note section

--- JP-BE-PY-batch-2/class_18/notes_app_flask_waqas/main.py
+++ JP-BE-PY-batch-2/class_18/notes_app_flask_waqas/main.py
@@ -2,12 +2,12 @@ from flask import Flask
 
 from route.auth import auth_bp
 from route.category import category_bp
-from route.notes import notes_bp
+# from route.notes import notes_bp
 
 def create_app():
   app = Flask(__name__)
   app.register_blueprint(auth_bp, url_prefix='/api')
-  app.register_blueprint(notes_bp, url_prefix='/api')
+  # app.register_blueprint(notes_bp, url_prefix='/api')
   app.register_blueprint(category_bp, url_prefix='/api')
     
   
--- JP-BE-PY-batch-2/class_18/notes_app_flask_waqas/route/notes.py
+++ JP-BE-PY-batch-2/class_18/notes_app_flask_waqas/route/notes.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+# from flask import Blueprint
 
-from controller.notes import create_notes,get_user_notes,delete_user_notes,update_user_notes
+# from controller.notes import create_notes,get_user_notes,delete_user_notes,update_user_notes
 
-notes_bp = Blueprint("notes_bp", __name__)
-notes_bp.add_url_rule('/notes',view_func=create_notes,methods=['POST'])
-notes_bp.add_url_rule('/notes',view_func=get_user_notes,methods=['GET'])
-notes_bp.add_url_rule('/notes/<id>',view_func=delete_user_notes,methods=['DELETE'])
-notes_bp.add_url_rule('/notes/<id>',view_func=update_user_notes,methods=['PUT'])
+# notes_bp = Blueprint("notes_bp", __name__)
+# notes_bp.add_url_rule('/notes',view_func=create_notes,methods=['POST'])
+# notes_bp.add_url_rule('/notes',view_func=get_user_notes,methods=['GET'])
+# notes_bp.add_url_rule('/notes/<id>',view_func=delete_user_notes,methods=['DELETE'])
+# notes_bp.add_url_rule('/notes/<id>',view_func=update_user_notes,methods=['PUT'])
